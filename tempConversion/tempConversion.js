08e006b1c0d1482ec4fc63d1383f909c9aeab448
peuchen <npeuchen@gmail.com>
2020-05-07 19:14:14 +0200

    Solve tempConversion exercise

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(degrees) {
+  let temp = (degrees - 32) * (5/9);
+  return parseFloat(temp.toFixed(1));
 }
 
-const ctof = function() {
-
+const ctof = function(degrees) {
+  let temp = degrees * (9/5) + 32;
+  return parseFloat(temp.toFixed(1));
 }
 
 module.exports = {
