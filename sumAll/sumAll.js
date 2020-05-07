d9604dcf2ddc2dfc5acc59ddfb34053b16858628
peuchen <npeuchen@gmail.com>
2020-05-07 18:02:22 +0200

    Solve sumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    let sum = 0;
 
+    if (typeof(startNum) != "number" || typeof(endNum) != "number") {
+        return "ERROR";
+    }
+    else if (startNum <0 || endNum <0) {
+        return "ERROR";
+    }
+    else if (startNum > endNum) {
+        for (i = endNum; i <= startNum; i++) {
+            sum += i;
+        }
+    }
+    else {
+        for (i = startNum; i <= endNum; i++) {
+            sum += i;
+        }
+    };
+    return sum;
 }
 
 module.exports = sumAll
